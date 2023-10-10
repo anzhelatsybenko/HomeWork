f40f8c06ff7c1e91759ef61d588a2b047dcd894f
anzhelatsybenko <a.tsybenko@semlex.com>
2023-10-10 10:37:21 +0200

    commit2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 "use strict"
-let unit, amount;
+
 // converter km to m
-unit = "km";
-amount = 10;
-if ((unit === "km") || (unit === "h") || (unit === "kg"))
-{
+let unit = "km", amount = 10;
+
     switch (unit) {
         case "km":
             console.log(amount + " " + unit + " is" + " " + amount*1000 + " m")
@@ -15,43 +13,41 @@ if ((unit === "km") || (unit === "h") || (unit === "kg"))
         case 'kg':
             console.log(amount + " " + unit + " is" + " " + amount*1000 + " g");
             break;
+        default :
+            console.log("unit is unknown");
     }
-} else console.log("unit is unknown");
+
 
 //converter hours to minutes
-unit = "h";
-amount = 1;
+unit = "h", amount = 1;
 
-if ((unit === "km") || (unit === "h") || (unit === "kg"))
-{
-    switch (unit) {
-        case "km":
-            console.log(amount + " " + unit + " is" + " " + amount*1000 + " m")
-            break
-        case 'h':
-            console.log(amount + " " + unit + " is" + " " + amount*60 + " min");
-            break;
-        case 'kg':
-            console.log(amount + " " + unit + " is" + " " + amount*1000 + " g");
-            break;
-    }
-} else console.log("unit is unknown");
+switch (unit) {
+    case "km":
+        console.log(amount + " " + unit + " is" + " " + amount*1000 + " m")
+        break
+    case 'h':
+        console.log(amount + " " + unit + " is" + " " + amount*60 + " min");
+        break;
+    case 'kg':
+        console.log(amount + " " + unit + " is" + " " + amount*1000 + " g");
+        break;
+    default :
+        console.log("unit is unknown");
+}
 
 //converter kg to g
-unit = "kg";
-amount = 1;
+unit = "kg", amount = 1;
 
-if ((unit === "km") || (unit === "h") || (unit === "kg"))
-{
-    switch (unit) {
-        case "km":
-            console.log(amount + " " + unit + " is" + " " + amount*1000 + " m")
-            break
-        case 'h':
-            console.log(amount + " " + unit + " is" + " " + amount*60 + " min");
-            break;
-        case 'kg':
-            console.log(amount + " " + unit + " is" + " " + amount*1000 + " g");
-            break;
-    }
-} else console.log("unit is unknown");
+switch (unit) {
+    case "km":
+        console.log(amount + " " + unit + " is" + " " + amount*1000 + " m")
+        break
+    case 'h':
+        console.log(amount + " " + unit + " is" + " " + amount*60 + " min");
+        break;
+    case 'kg':
+        console.log(amount + " " + unit + " is" + " " + amount*1000 + " g");
+        break;
+    default :
+        console.log("unit is unknown");
+}
