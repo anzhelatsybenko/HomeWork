1e9467a0fcca5e9e3549b897209e158a0e36817f
anzhelatsybenko <a.tsybenko@semlex.com>
2023-10-17 15:37:07 +0200

    commit1

--- js/script.js
+++ js/script.js
@@ -1,46 +1,63 @@
 "use strict"
-//----------Home Work - Triangle----------
-
-// --Version 1 - with "for"--
-function triangle_v1 (heigt, symbol){
-    let s = symbol;
-    for (let i = 1; i<=heigt; i++) {
-            console.log(symbol);
-            symbol = symbol + s;
+//----------Home Work - isNaN----------
+//For verification : null, undefined, 20, "test", NaN, true, "test"/20, "123", "12,12", "12.12"
+function isNotnumber (v) {
+    if (v!==null && v!==true && v!== false)
+    {
+        if (Number(v) !== Number(v)) console.log("true");
+        else console.log("false");
     }
+else console.log("false");
 }
 
-triangle_v1(5,"+");
+let v = 20;
+isNotnumber(v);
+console.log(isNaN(v));
 
-// --Version 2 - with "while"--
-/*function triangle_v2 (heigt, symbol){
-    let s = symbol;
-    let i = 1;
-    while (i<=heigt) {
-        console.log(symbol);
-        symbol = symbol + s;
-        i++;
-    }
+//----------Home Work - Pad----------
+
+/*function pad (str, symb, numbOfsymb, direction) {
+
+   let str_symb = symb.repeat(numbOfsymb-str.length);
+
+   if (direction === false) return str.concat(str_symb);
+   else return str_symb.concat (str);
+
 }
 
-triangle_v2(5,"+");*/
+console.log (pad ("test", "*", 10,false));*/
 
-//----------Home Work - Summary----------
-/*function summary () {
-    let sum = 0;
-    for (let i = 1; i <= 100; i++) {
-        if (i%3) sum = sum + i;
-    }
-    return sum;
-}
-console.log(summary ());*/
+//----------Home Work - checkProbabilityTheory ----------
 
-//----------Home Work - Pow----------
-/*function pow (numb, pow) {
-    let n = numb;
-    for (let i = 1; i < pow; i++) {
-        numb = numb*n;
+/*function checkProbabilityTheory (count) {
+    //count_odd : Number of odd numbers; count_even : Number of even numbers.
+    let count_odd = 0, count_even = 0, res;
+
+    //Cycle to generate random numbers
+    for (let i = 1; i <= count; i++) {
+        //round : rounded random integer
+        let round = Math.round(Math.random() * 1000);
+        //Selection of numbers from 100 to 1000
+        if (round < 100 || round > 1000) i--;
+        else {
+            //Check for parity of a number and count the number of even and odd numbers
+            if (round % 2) count_odd++;
+            else count_even++;
+        }
     }
-    return numb;
+    //Check whether the parcentage is close to 50%
+    if (Math.round(count_even / count * 100) >= 40 && Math.round(count_even / count * 100) <= 60)
+        //Create result string
+        return res = "Total numbers : " + count + "\n" +
+            "Number of even numbers : " + count_even + "\n" +
+            "Number of odd numbers : " + count_odd + "\n" +
+            "Percentage of even to odd numbers : " + Math.round(count_even / count * 100) + " % " + Math.round(count_odd / count * 100) + "\n" +
+            "Percentage ratio is close to 50%50.";
+    else return res = "Total numbers : " + count + "\n" +
+        "Number of even numbers : " + count_even + "\n" +
+        "Number of odd numbers : " + count_odd + "\n" +
+        "Percentage of even to odd numbers : " + Math.round(count_even / count * 100) + " % " + Math.round(count_odd / count * 100) + "\n" +
+        "Percentage ratio is not close to 50%50.";
+
 }
-console.log(pow (3,2));*/
+console.log(checkProbabilityTheory(100));*/
