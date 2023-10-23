1afc309310a30710048e6bfc563c41ebb741aa59
anzhelatsybenko <a.tsybenko@semlex.com>
2023-10-23 20:07:57 +0200

    commit1

--- js/script.js
+++ js/script.js
@@ -1,63 +1,76 @@
 "use strict"
-//----------Home Work - isNaN----------
-//For verification : null, undefined, 20, "test", NaN, true, "test"/20, "123", "12,12", "12.12"
-function isNotnumber (v) {
-    if (v!==null && v!==true && v!== false)
-    {
-        if (Number(v) !== Number(v)) console.log("true");
-        else console.log("false");
-    }
-else console.log("false");
-}
+//----------Home Work - GetInfo()----------
 
-let v = 20;
-isNotnumber(v);
-console.log(isNaN(v));
-
-//----------Home Work - Pad----------
-
-/*function pad (str, symb, numbOfsymb, direction) {
-
-   let str_symb = symb.repeat(numbOfsymb-str.length);
-
-   if (direction === false) return str.concat(str_symb);
-   else return str_symb.concat (str);
-
-}
-
-console.log (pad ("test", "*", 10,false));*/
-
-//----------Home Work - checkProbabilityTheory ----------
-
-/*function checkProbabilityTheory (count) {
-    //count_odd : Number of odd numbers; count_even : Number of even numbers.
-    let count_odd = 0, count_even = 0, res;
-
-    //Cycle to generate random numbers
-    for (let i = 1; i <= count; i++) {
-        //round : rounded random integer
-        let round = Math.round(Math.random() * 1000);
-        //Selection of numbers from 100 to 1000
-        if (round < 100 || round > 1000) i--;
-        else {
-            //Check for parity of a number and count the number of even and odd numbers
-            if (round % 2) count_odd++;
-            else count_even++;
+var legoset = {
+    NameOfSet: "4 Privet Drive",
+    NameOfCollection: "Harry Potter",
+    NumberOfPieces: '797',
+    GetInfo() {
+        for (let key in legoset) {
+            console.log(key + ': ' + legoset[key]);
         }
     }
-    //Check whether the parcentage is close to 50%
-    if (Math.round(count_even / count * 100) >= 40 && Math.round(count_even / count * 100) <= 60)
-        //Create result string
-        return res = "Total numbers : " + count + "\n" +
-            "Number of even numbers : " + count_even + "\n" +
-            "Number of odd numbers : " + count_odd + "\n" +
-            "Percentage of even to odd numbers : " + Math.round(count_even / count * 100) + " % " + Math.round(count_odd / count * 100) + "\n" +
-            "Percentage ratio is close to 50%50.";
-    else return res = "Total numbers : " + count + "\n" +
-        "Number of even numbers : " + count_even + "\n" +
-        "Number of odd numbers : " + count_odd + "\n" +
-        "Percentage of even to odd numbers : " + Math.round(count_even / count * 100) + " % " + Math.round(count_odd / count * 100) + "\n" +
-        "Percentage ratio is not close to 50%50.";
-
 }
-console.log(checkProbabilityTheory(100));*/
+
+console.log (" ");
+legoset.GetInfo();
+
+legoset.NewProperty = "New property";
+
+console.log (" ");
+legoset.GetInfo();
+
+//----------Home Work - Services----------
+
+/*var services = {
+    haircut: "60 euro",
+    shaving: "80 euro",
+    shampooing: "100 euro",
+    price() {
+        var sum = 0;
+        for (let key in services) {
+            if (key === "price" || key === "minPrice" || key === "maxPrice") continue;
+            else sum = sum + Number(services[key].slice(0,services[key].indexOf(" ")));
+        }
+        return sum;
+    },
+    minPrice () {
+        var min = Number(services.haircut.slice(0, services.haircut.indexOf(" ")));
+        for (let key in services) {
+            if (key === "price" || key === "minPrice" || key === "maxPrice") continue;
+            else {
+                if (Number(services[key].slice(0, services[key].indexOf(" "))) < min) {
+                    min = Number(services[key].slice(0, services[key].indexOf(" ")));
+                }
+            }
+        }
+        return min;
+    },
+    maxPrice () {
+        var max = Number(services.haircut.slice(0, services.haircut.indexOf(" ")));
+        for (let key in services) {
+            if (key === "price" || key === "minPrice" || key === "maxPrice") continue;
+            else {
+                if (Number(services[key].slice(0, services[key].indexOf(" "))) > max) {
+                    max = Number(services[key].slice(0, services[key].indexOf(" ")));
+                }
+            }
+        }
+        return max;
+    }
+}
+
+console.log (" ");
+console.log ("Before adding a new property");
+console.log ("Total price : "+services.price()+" euro");
+console.log ("Minimal price : "+services.minPrice()+" euro");
+console.log ("Maximum price : "+services.maxPrice()+" euro");
+
+services.manicure = "40 euro";
+services.pedicure = "200 euro";
+
+console.log (" ");
+console.log ("After adding a new property");
+console.log ("Total price : "+services.price()+" euro");
+console.log ("Minimal price : "+services.minPrice()+" euro");
+console.log ("Maximum price : "+services.maxPrice()+" euro");*/
